18d6f345d1e137d33d730c648ddfeb70f1e7c2ed
PavanK152 <neikora0245@gmail.com>
2026-02-11 17:53:44 +0530

    Static data version

--- src/hooks.js
+++ src/hooks.js
@@ -1,34 +1,30 @@
 import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { setCoursedata } from "./store/udemySlice";
+import { courseData as staticCourseData } from "./data/courseData";
 
+// 🔥 Fetch Courses (Now Using Static Data)
 export function useFetchCourseData() {
   const dispatch = useDispatch();
-  const courseData = useSelector((store) => store.udemy.courses);
-
-  async function getData() {
-    if (courseData.length !== 0) return;
-    let apiData = await fetch(
-      `https://mocki.io/v1/be0822f2-200f-41ea-b333-aa465c3bb8e7`
-    );
-    let jsonData = await apiData.json();
-    dispatch(setCoursedata(jsonData));
-  }
+  const storedCourses = useSelector((store) => store.udemy.courses);
 
   useEffect(() => {
-    getData();
-  }, []);
+    if (storedCourses.length === 0) {
+      dispatch(setCoursedata(staticCourseData));
+    }
+  }, [dispatch, storedCourses]);
 
-  return courseData;
+  return storedCourses;
 }
 
+// 🔥 Check Wishlist
 export function useIsCourseExistInWishList(id) {
   const wishListData = useSelector((state) => state.udemy.wishlistCourse);
-  const courseIdx = wishListData.findIndex((data) => data.id == id);
-  return courseIdx == -1 ? false : true;
+  return wishListData.some((data) => data.id === id);
 }
+
+// 🔥 Check Cart
 export function useIsCourseExistInCart(id) {
   const cartData = useSelector((state) => state.udemy.cartCourses);
-  const courseIdx = cartData.findIndex((data) => data.id == id);
-  return courseIdx == -1 ? false : true;
+  return cartData.some((data) => data.id === id);
 }
